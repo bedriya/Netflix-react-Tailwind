450f822332ee217168db93f5f589b94bf5792cfc
bedriya <111170324+bedriya@users.noreply.github.com>
2022-12-29 01:26:03 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { AuthContextProvider } from "./Context/AuthContext";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Account from "./Pages/Account";
+import ProtectedRoute from "./Components/ProtectedRoute";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthContextProvider>
+
+
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>} />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
